42179861530fb807a0e65129a906a661d960d192
Tim Grace <tgr@barton.ac.uk>
2026-06-30 21:41:27 +0100

    Install BetterAuth ready for login

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,25 +1,27 @@
 import { Link } from "@tanstack/react-router";
 
+
 export default function Header() {
   const links = [{ to: "/", label: "Home" },
     {to: "https://food.tgrace.dev", label: "Food"},
     {to: "https://www.tgrace.dev", label: "Sims"},
   ] as const;
-
+  
   return (
     <div>
       <div className="flex flex-row items-center justify-between px-2 py-1">
         <nav className="flex gap-4 text-lg">
           {links.map(({ to, label }) => {
-            return to[0]=="/"?(
+            if (to[0]=="/") return (
               <Link key={to} to={to as any}>
                 {label}
               </Link>
-            ):(
+            )
+            if (to[0]=="h") return (
               <a key={to} href={to}>
                 {label}
               </a>
-            );
+            )
           })}
         </nav>
         <div className="flex items-center gap-2"></div>
