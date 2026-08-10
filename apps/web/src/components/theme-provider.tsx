ed9cdb17491704b52463d7b68a62ccc6798be008
Tim Grace <timggrace@gmail.com>
2026-08-10 06:51:31 +0000

    Style sidebar headings. Remove redundant styles

--- apps/web/src/components/theme-provider.tsx
+++ apps/web/src/components/theme-provider.tsx
@@ -18,7 +18,20 @@ function getThemeScript(storageKey: string, defaultTheme: Theme) {
     const key = JSON.stringify(storageKey)
     const fallback = JSON.stringify(defaultTheme)
 
-    return `(function(){try{var t=localStorage.getItem(${key});if(t!=='light'&&t!=='dark'&&t!=='system'){t=${fallback}}var d=matchMedia('(prefers-color-scheme: dark)').matches;var r=t==='system'?((d?'dark':'light'):t;var e=document.documentElement;e.classList.add(r);e.style.colorscheme=r}catch(e){}})();`
+    return `(function(){
+        try{
+            var t=localStorage.getItem(${key});
+            if(t!=='light'&&t!=='dark'&&t!=='system'){
+                t=${fallback}
+            }
+            var d=matchMedia('(prefers-color-scheme: dark)').matches;
+            var r=t==='system'?(d?'dark':'light'):t;
+            var e=document.documentElement;
+            e.classList.add(r);
+            e.style.colorscheme=r
+        }catch(e){}
+        }
+        )();`
 }
 
 const ThemeProviderContext = createContext<ThemeProviderState>({
